4c18d5cab1c98b0ae12cc373e4db2a0ffb16d9fe
Marshall Hamilton <marshall.m.hamilton@gmail.com>
2026-08-09 09:36:32 -0400

    fixed startup maybe

--- startup.lua
+++ startup.lua
@@ -9,7 +9,9 @@ _G.Textbox = require("WinBlocks.text")
 _G.Event = require("WinBlocks.event")
 _G.Screen = require("WinBlocks.screen")
 _G.Shape = require("WinBlocks.shape")
-
+for index, thing in pairs(_G.Textbox) do
+    write(index)
+end
 _G.Screen.Init()
 _G.Screen.RunScreen()
 
