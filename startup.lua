dea7db41f564811779f12fd5a1e4228e060d1802
Marshall Hamilton <marshall.m.hamilton@gmail.com>
2026-08-09 09:49:30 -0400

    fixed startup maybe

--- startup.lua
+++ startup.lua
@@ -1,14 +1,12 @@
-_G.term = term
-_G.os = os
-
 term.clear()
 term.setCursorPos(1,1)
 
 
 _G.Textbox = require("WinBlocks.text")
-_G.Event = require("WinBlocks.event")
+_G.Connection,_G.Event = require("WinBlocks.event")
 _G.Screen = require("WinBlocks.screen")
 _G.Shape = require("WinBlocks.shape")
+
 for index, thing in pairs(_G.Textbox) do
     write(index.."\n")
 end
