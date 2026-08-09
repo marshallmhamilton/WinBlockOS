97273c5e157bbcd0e8d14dfb68814fe3adab0a12
Marshall Hamilton <marshall.m.hamilton@gmail.com>
2026-08-08 22:37:55 -0400

    initial commit

---
+++ startup.lua
@@ -0,0 +1,18 @@
+_G.error = false
+
+local width, height = term.getSize()
+
+local text = "hello world"
+
+while not _G.error do
+
+    local e,p = os.pullEvent()
+
+    --render loop
+    term.clear()
+    term.setCursorPos( (width - string.len(text))/2,height/2)
+    term.write(text)
+    sleep()
+end
+
+
