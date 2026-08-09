0225c78d604b3ba6698b30753df797fff56c1927
Marshall Hamilton <marshall.m.hamilton@gmail.com>
2026-08-09 02:00:02 -0400

    fixed startup

--- startup.lua
+++ startup.lua
@@ -4,6 +4,11 @@ _G.os = os
 term.clear()
 term.setCursorPos(1,1)
 
+require("WinBlocks.event")
+require("WinBlocks.screen")
+require("WinBlocks.shape")
+require("WinBlocks.text")
+
 _G.Screen.Init()
 _G.Screen.RunScreen()
 
