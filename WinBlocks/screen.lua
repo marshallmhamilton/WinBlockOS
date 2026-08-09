7289751d877e7dba8a574ea64eb6b3182e427630
Marshall Hamilton <marshall.m.hamilton@gmail.com>
2026-08-09 12:46:56 -0400

    did physik

--- WinBlocks/screen.lua
+++ WinBlocks/screen.lua
@@ -7,7 +7,7 @@ local Screen = {
 function Screen.Init()
     term.setGraphicsMode(2)
     Screen.Text = _G.Textbox.new("Hello World",{x=10,y=10},1)
-    Screen.Text.Velocity = {100,0}
+    Screen.Text.Velocity = {x=100,y=0}
     Screen.Text.Moves = true
     table.insert(Screen.Objects,Screen.Text)
     Screen.Width, Screen.Height = term.getSize(2)
--- WinBlocks/textbox.lua
+++ WinBlocks/textbox.lua
@@ -85,7 +85,7 @@ function Textbox.new(str, pos, scale)
     local self = setmetatable({},Textbox)
     self.String = str
     self.Moves = false
-    self.Velocity = {0,0}
+    self.Velocity = {x=0,y=0}
     self.Scale = scale or 1
     self.Position = pos or {x=0,y=0}
     return self
