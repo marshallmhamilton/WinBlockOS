80e17e87c859bf74ddf85845c319c14cd5edf633
Marshall Hamilton <marshall.m.hamilton@gmail.com>
2026-08-09 10:33:05 -0400

    fixed startup maybe

--- WinBlocks/screen.lua
+++ WinBlocks/screen.lua
@@ -7,7 +7,7 @@ function Screen.DrawBottomSqaure(shape)
 end
 
 function Screen.Init()
-    term.setGraphicsMode(2)
+    term.setGraphicsMode(1)
    
     table.insert(Screen.Objects,_G.Textbox.new("hi",{x=10,y=10},1))
 end
--- WinBlocks/shape.lua
+++ WinBlocks/shape.lua
@@ -6,7 +6,7 @@ local Shape = {
 }
 
 function Shape.DrawSquare(x,y,scale)
-    term.drawPixels(x,y,255,scale,scale)
+    term.drawPixels(x,y,colors.white,scale,scale)
 end
 
 return Shape
